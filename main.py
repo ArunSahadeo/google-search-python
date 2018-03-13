44121d5dc5571826adc77a50c0476984afaa7b28
Arun Sahadeo <arun@wirebox.co.uk>
2018-03-13 10:48:32 +0000

    Filter latest query

--- main.py
+++ main.py
@@ -1,8 +1,47 @@
 #!/usr/bin/env python3 -u
 
-import datetime, json, os, sys, time
+import datetime, json, operator, os, sys, time
 import sqlite3
 import shutil
+
+def getLatestResult(latest_query):
+    browser = None
+
+    if "chromium" in str(latest_query).lower():
+        browser = "chromium"
+    elif "chrome" in str(latest_query).lower():
+        browser = "chrome"
+    elif "firefox" in str(latest_query).lower():
+        browser = "firefox"
+
+    now = time.time()
+    one_week_ago = now - (60 * 60 * 24 * 7)
+
+    if latest_query['modification_time'] < one_week_ago:
+        return
+
+    old_filename = os.path.basename(latest_query['path'])
+    shutil.copy(latest_query['path'], os.getcwd())
+    src_file = os.path.join(os.getcwd(), old_filename)
+    dst_file = str("%s_browser_history" % (browser))
+    new_file = os.path.join(os.getcwd(), dst_file)
+    os.replace(src_file, dst_file)
+
+    if not os.path.isfile(new_file):
+        return
+
+    conn = sqlite3.connect(new_file)
+    cursor = conn.cursor()
+
+    try:
+        if browser == "chromium" or browser == "chrome":
+            cursor.execute('SELECT title from urls WHERE title LIKE "%Google Search%" ORDER BY id DESC LIMIT 1')
+        elif browser == "firefox":
+            cursor.execute('SELECT title FROM moz_places WHERE title LIKE "%Google Search%" ORDER BY id DESC LIMIT 1')
+        select_result = cursor.fetchone()[0]
+        print(select_result)
+    except Exception as error:
+        print(error)
 
 def checkConfig(config_file):
     if not os.path.isfile(config_file):
@@ -37,7 +76,7 @@ def checkConfig(config_file):
                 history_path = history_path.replace('$PROFILE_FOLDER', firefox_profile)
             if len(history_path) < 1 or not os.path.isfile(history_path):
                 continue
-            browser_sqlite_dbs.append(history_path)
+            browser_sqlite_dbs.append({ 'path': history_path, 'modification_time': os.path.getmtime(history_path) })
 
 browser_sqlite_dbs = []
 
@@ -52,42 +91,6 @@ if not len(browser_sqlite_dbs) > 0:
     print("No SQLite files found!")
     sys.exit(0)
 
-for index, db_path in enumerate(browser_sqlite_dbs):
-    db_timestamp = os.path.getmtime(db_path)
-    browser = None
-    
-    if "chromium" in str(db_path).lower():
-        browser = "chromium"
-    elif "chrome" in str(db_path).lower():
-        browser = "chrome"
-    elif "firefox" in str(db_path).lower():
-        browser = "firefox"
+latest_query = max(browser_sqlite_dbs, key=operator.itemgetter('modification_time'))
 
-    now = time.time()
-    one_week_ago = now - (60 * 60 * 24 * 7)
-
-    if db_timestamp < one_week_ago:
-        continue
-
-    old_filename = os.path.basename(db_path)
-    shutil.copy(db_path, os.getcwd())
-    src_file = os.path.join(os.getcwd(), old_filename)
-    dst_file = str("%s_browser_history" % (browser))
-    new_file = os.path.join(os.getcwd(), dst_file)
-    os.replace(src_file, dst_file)
-
-    if not os.path.isfile(new_file):
-        continue
-
-    conn = sqlite3.connect(new_file)
-    cursor = conn.cursor()
-
-    try:
-        if browser == "chromium" or browser == "chrome":
-            cursor.execute('SELECT title from urls WHERE title LIKE "%Google Search%" ORDER BY id DESC LIMIT 1')
-        elif browser == "firefox":
-            cursor.execute('SELECT title FROM moz_places WHERE title LIKE "%Google Search%" ORDER BY id DESC LIMIT 1')
-        select_result = cursor.fetchone()[0]
-        print(select_result)
-    except Exception as error:
-        print(error)
+getLatestResult(latest_query)
